40e84b9b7d409e23347b7a1ec3af7f944871950e
chdavid8888 <168498652+chdavid8888@users.noreply.github.com>
2024-06-28 11:14:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ if __name__ == '__main__' :
   main()
 
 def main() : 
-  st.header('Halaman Steamlit David')
+  st.header('Halaman Streamlit David')
   st.subheader('This is SubHeader')
   st.markdown('# Rendering Markdown ')
   st.write('Some Phytagorean Equation : ')
