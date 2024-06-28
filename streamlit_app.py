4c888cdfb202a2f2ad7df3ba791a1219b5334908
chdavid8888 <168498652+chdavid8888@users.noreply.github.com>
2024-06-28 11:09:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st 
 import pandas as pd 
-#import requests
-#from st_aggrid import AgGrid
+import requests
+from st_aggrid import AgGrid
 
 house = pd.read_csv('house_clean.csv')
 
@@ -17,6 +17,8 @@ def main() :
   st.markdown('# Rendering Markdown ')
   st.write('Some Phytagorean Equation : ')
   st.latex('c^2 = a^2+b^2')
+
+  st.dataframe(house)
 
 if __name__ == '__main__' : 
   main()
