981c2510a5ec8da91adf2d70886132be5611c957
chdavid8888 <168498652+chdavid8888@users.noreply.github.com>
2024-06-28 13:21:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,5 +21,8 @@ def main() :
   st.dataframe(house)
   st.write('Metrics')
   st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+
+  st.write('Menampilkan Dataframe dengan St AgGrid')
+  AgGrid(titanic)
 if __name__ == '__main__' : 
   main()
