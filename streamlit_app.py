f1c87d5eedc0da0ced976d68ba6df37ae373f9e9
chdavid8888 <168498652+chdavid8888@users.noreply.github.com>
2024-06-28 14:11:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,9 +82,9 @@ def main() :
 
   st.write("Outside the form")
 
-tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
-tab1.write("this is tab 1")
-tab2.write("this is tab 2")
+  tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
+  tab1.write("this is tab 1")
+  tab2.write("this is tab 2")
 
 
 if __name__ == '__main__' : 
